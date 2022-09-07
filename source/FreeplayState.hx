627401f8266502c20af6613d428311d1856de062
MENDINGOU <110300418+MENDINGOU@users.noreply.github.com>
2022-09-07 18:37:22 -0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -272,6 +272,10 @@ class FreeplayState extends MusicBeatState
 
 		charInputs = "";
 
+		#if android
+                addVirtualPad(LEFT_FULL, A_B);
+                #end
+		
 		super.create();
 	}
 
